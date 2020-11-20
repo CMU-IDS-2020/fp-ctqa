f260d3507670dcb99857edbaf6827d584b3fc6bf
MichaelYxWang <yuanxin.wang97@gmail.com>
2020-11-20 16:18:20 -0500

    add randomness for initial tweets

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@
 import numpy as np
 import time
 import pandas as pd
+import random
 # import torch
 # import torch.nn as nn
 import streamlit as st
@@ -35,7 +36,9 @@ tweets = df.text.tolist()
 num_tweets = len(tweets)
 
 st.subheader("Here are some randomly selected most recent tweets about Covid-19")
-for i, tweet in enumerate(tweets[num_tweets - 10: num_tweets - 5]):
+indices = random.sample(range(num_tweets), 5)
+
+for i, tweet in enumerate([tweets[i] for i in indices]):
     st.write("[{}] ".format(i+1) + tweet)
 
 def get_top_n_idx(A, N):
@@ -45,28 +48,19 @@ def get_top_n_idx(A, N):
     best_scores = A[col_idx,sorted_row_idx]
     return sorted_row_idx, best_scores
 
-
 sample_ids = [1,2,3,4,5]
-#
-# id_to_text = {}
-# sample_ids = [1322748291646476288, 1322748291751227397, 1322748291839262723, 1322748292384645120, 1322748296432144385, 1322748297736454144, 1322748298797682690, 1322748301842747392, 1322748304946614273, 1322748305898573825]
-# for n in sample_ids:
-#     optimal_tweets = []
-#     for i in range(5):
-#         # optimal_tweets.append(get_t_value(n, index)) # This can be a function.
-#         optimal_tweets.append(str(n) + " has its text as number " + str(i+1))
-#     id_to_text[n] = optimal_tweets
-# metric_choices = [id_to_text[name_id] for name_id in sample_ids]
 
 st.subheader("Which tweet would you like to get information on?")
 st.write("Please select the tweet id based on the number inside [] above")
 tweet_option = st.selectbox('', sample_ids)
 tweet_option -= 1
+st.write("Here is the tweet you selected!")
+st.write([tweets[i] for i in indices][tweet_option])
 st.subheader("How many similar tweets would you like to retrieve?")
 n_opt = st.slider("", min_value=1, max_value=5, value=3, step=1)
 
 
-sorted_row_idx, best_scores = get_top_n_idx(all_scores[num_tweets - 10: num_tweets - 5], n_opt)
+sorted_row_idx, best_scores = get_top_n_idx(all_scores[indices], n_opt)
 sorted_row_idx = sorted_row_idx[tweet_option].tolist()[::-1][1:]
 best_scores = best_scores[tweet_option].tolist()[::-1][1:]
 
