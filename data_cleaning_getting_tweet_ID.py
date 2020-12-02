b6d17b69ca94c8d7526b3a19734442171e793275
yuan-ds <54598449+yuan-ds@users.noreply.github.com>
2020-12-02 13:22:47 -0500

    Add files via upload
    sample 100,000 tweets

--- data_cleaning_getting_tweet_ID.py
+++ data_cleaning_getting_tweet_ID.py
@@ -8,18 +8,59 @@
 import pandas as pd
 
 
-# In[3]:
+# In[2]:
 
 
 # read the tsv file 
 # the orginal tsv file is over 9 GB; we didn't upload it to the github
 tsv_read = pd.read_csv("full_dataset_clean.tsv", sep='\t')
-# get the most recent 2000 tweets
-tweets_data = tsv_read.tail(2000)
-# filter the english tweets
-tweets_english = tweets_data[tweets_data['lang'] == 'en']
-# get the tweet_id and write it to csv file
-tweets_english['tweet_id'].to_csv("id.csv", index=False, header=False) 
-# the id.csv file is uploaded to the github
+
+
+# In[6]:
+
+
+tsv_read.shape
+#(205701547, 5)
+#total 205701547 tweets
+
+
+# In[7]:
+
+
+# english tweets total
+tweets_english = tsv_read[tsv_read['lang'] == 'en']
+
+
+# In[8]:
+
+
+tweets_english.shape
+#(116714566, 5)  total 116714566 tweets
+
+
+# In[11]:
+
+
+# random sample 100,000 tweets out of the 116714566 english tweets
+
+sample_tweets = tweets_english.sample(n=100000,replace=False,random_state=1)
+
+
+# In[15]:
+
+
+# sort the date
+sample_tweets = sample_tweets.sort_values(by=['date'])
+
+
+# In[17]:
+
+
+# outout the whole dataframe to csv
+sample_tweets.to_csv("sample_ID_info_100000.csv", index = False)
+# output the tweet_id to csv for hydrator
+sample_tweets['tweet_id'].to_csv("id_100000_sample.csv", index=False, header=False) 
+
+# the two csvs are uploaded to the github
 # next step is upload the id.csv file to Hydrator to get the hydrated tweets
 
