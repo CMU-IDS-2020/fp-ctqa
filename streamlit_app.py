6b274a4576005b254660bb3f436d5ca064b7d717
MichaelYxWang <yuanxin.wang97@gmail.com>
2020-12-08 12:35:33 -0500

    Progress on descriptions

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,9 +54,15 @@ def get_best_match_ner(question, n_opt):
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 st.title('Covid-19 Twitter Search')
 st.header("Introduction")
-st.success("[TODO - Yuanxin] introductory paragraph displayed here")
+st.success("Since the outbreak of COVID-19 in early 2020, people have been searching frequently on social media platforms such as Twitter about health issues. \
+           The vast amount of tweets has also made it difficult for people to quickly locate their content of interest. \
+           Using Twitter hashtags alone to label tweets still leaves an enormous corpus. \
+           In this situation, it is natural to ask the following question: how do we help people filter related tweets with more efficiency and flexibility?\n\
+           \nWe want to approach this search problem from a different perspective: instead of performing search using keywords\
+           , we find it more natural for the users to have the following user story flow: \
+           look at a few recent tweets, find the one they are more intersted in, and then dig into similar contents.")
 
-st.header("Dataset and Data Pipeline")
+st.header("Simple Explotory Data Analysis")
 st.success("[TODO - Yuanyuan] we can include the source of data, sample tweets, and a flow chart for data preprocessing")
 
 
@@ -81,21 +87,21 @@ st.image(pics[pic], use_column_width=True, width = 600)
 sample_ids = [1,2,3,4,5]
 tweet_option = int(pic[-2]) - 1
 st.subheader("How many similar tweets would you like to retrieve?")
-n_opt = st.slider("", min_value=1, max_value=5, value=3, step=1)
+n_opt = st.slider("", min_value=2, max_value=6, value=3, step=1)
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # NER Model
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-st.sidebar.write("Model Explanations")
-if st.sidebar.button("NER"):
-    st.sidebar.write("Named Entity Recognition uses a small Convolutional Neural Network")
-    st.sidebar.write("It is trained on generic English web text and performs poorly on certain tweets")
-    st.sidebar.write("Each document is broken down into Tokens with several [Attributes](https://spacy.io/api/token#attributes)")
-    st.sidebar.markdown("Tweets are matched based on [Jaccard Overlap](https://news.developer.nvidia.com/similarity-in-graphs-jaccard-versus-the-overlap-coefficient/)")
-if st.sidebar.button("Sentence Encoder"):
-    st.sidebar.markdown("The Sentence Encoder uses Long-Term Short Term - a type of [Recurrent Neural Net designed for sequences](https://arxiv.org/pdf/1705.02364.pdf)")
-    st.sidebar.write("It is trained on the Stanford Natural Language Inference Set")
-    st.sidebar.write("This transfer learning method generalizes well to new sequences")
+# st.sidebar.write("Model Explanations")
+# if st.sidebar.button("NER"):
+#     st.sidebar.write("Named Entity Recognition uses a small Convolutional Neural Network.")
+#     st.sidebar.write("It is trained on generic English web text and performs poorly on certain tweets")
+#     st.sidebar.write("Each document is broken down into Tokens with several [Attributes](https://spacy.io/api/token#attributes)")
+#     st.sidebar.markdown("Tweets are matched based on [Jaccard Overlap](https://news.developer.nvidia.com/similarity-in-graphs-jaccard-versus-the-overlap-coefficient/)")
+# if st.sidebar.button("Sentence Encoder"):
+#     st.sidebar.markdown("The Sentence Encoder uses Long-Term Short Term - a type of [Recurrent Neural Net designed for sequences](https://arxiv.org/pdf/1705.02364.pdf)")
+#     st.sidebar.write("It is trained on the Stanford Natural Language Inference Set")
+#     st.sidebar.write("This transfer learning method generalizes well to new sequences")
 
 sentence_sets = []
 with open('data/ners.pkl', 'rb') as f:
@@ -105,14 +111,19 @@ question = [tweets[i] for i in indices][tweet_option]
 doc, tokens, matches = get_best_match_ner(question, n_opt+1)
 
 
-st.subheader("Baseline Model - NER Model Result:")
+st.header("Baseline Model - NER Model")
+st.success("Our baseline Named Entity Recognition model uses a small Convolutional Neural Network. \
+           It is trained on generic English web text and performs poorly on certain tweets. \
+           Each document is broken down into Tokens with several [Attributes](https://spacy.io/api/token#attributes). \
+           Tweets are matched based on [Jaccard Overlap](https://news.developer.nvidia.com/similarity-in-graphs-jaccard-versus-the-overlap-coefficient/). \
+           We also provided the users with some explorations on which token attributes contribute most to the similarity score.")
 st.write('Here are the ordered top ' + str(n_opt) + ' tweets similar to this tweet BY NER tag overlap:')
 for tweet_idx in matches:
     if(tweet_idx != indices[tweet_option]):
         st.info(tweets[tweet_idx])
         st.write("\n")
 
-st.subheader("Which token attributes contribute most to the similarity socre?")
+st.subheader("Which token attributes contribute most to the similarity score?")
 Attributes = st.multiselect('Select token attributes to display',[
         "idx",
         "text",
@@ -136,7 +147,14 @@ if st.button("Show token attributes"):
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 st.header("Our Model - Setence Encoder Model")
+st.success("We use Facebook Infersent Natural Language Inference model to encode the the tweets to 4096-way vectors\
+            and then use vector level cosine similarity scoring to compute top-n similar tweets related to one tweet.\
+            An overview of Natural Language Inference architre is as follows and you can find more [more details here](https://research.fb.com/wp-content/uploads/2017/09/emnlp2017.pdf). \
+            We train the whole classification model end to end but only use the weights up to sentence encoder part to encode the tweets. \
+            \n\nFor training set, we use [Quora Question Pair dataset](https://www.kaggle.com/c/quora-question-pairs) and [SNLI dataset](https://nlp.stanford.edu/projects/snli/) with highly relevant to daily life / common sense sentence pairs, \
+            which is similar to twitter content. We also particularly pick the [Twitter Corpus version of the Glove](http://nlp.stanford.edu/data/glove.twitter.27B.zip) word embedding to improve the accuracy of our model.")
 
+st.image("pictures/nli.png", width = 550, caption='Infersent Architecture')
 st.subheader("Now please select your hyperparameters")
 learning_rate = float(st.radio("Choose Model Learning Rate", ('1e-5', '2e-5')))
 batch_size =int(st.radio("Choose Model Batch Size", ('64', '32')))
@@ -146,18 +164,16 @@ sorted_row_idx, best_scores = get_top_n_idx(all_scores[indices], n_opt)
 sorted_row_idx = sorted_row_idx[tweet_option].tolist()[::-1][1:]
 best_scores = best_scores[tweet_option].tolist()[::-1][1:]
 
-st.subheader("Our Model - Setence Encoder Model Result:")
+st.subheader("Our Model - Sentence Encoder Model Result:")
 st.write('Here are the ordered top ' + str(n_opt) + ' tweets similar to this tweet:')
 
 for tweet_idx, score in zip(sorted_row_idx, best_scores):
-    st.info(tweets[tweet_idx])
     display_score = round(float(1 + np.log(score)), 5)
-    st.write("with similarity score ")
-    st.write(display_score)
+    st.info(tweets[tweet_idx] + "\n\n [similarity score: " + str(display_score) + "]")
     st.write("\n")
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Conclusions and future work
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
+st.header("Conclusion and Analysis")
 st.write("You can see our fine tuned model performs better than the NER baseline model because the output tweets are more similar")
